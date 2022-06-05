637e17438bc76d3707ca6645097043b8c5047779
Rana-TANRIKULU <ranatanrikuluu@gmail.com>
2022-06-05 23:33:50 +0300

    css dosyası ekledim, değişken tanımladım

---
+++ script.js
@@ -0,0 +1,28 @@
+// console.log("hello me");
+// console.log("2143");
+// console.log(true)
+// console.log([1,2,3]);
+// console.error("hata-var-hata-var");
+// console.warn("uyarı çok tatlısınız");
+
+// //tek satir
+
+// /*
+
+// çok fazla satır
+
+// */
+
+// console.clear();
+
+
+
+//Değişkenler
+
+
+var age;
+console.log(age);
+
+age=11
+console.log(age);
+
